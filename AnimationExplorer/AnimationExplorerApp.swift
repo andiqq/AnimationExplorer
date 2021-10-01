4f140fd478e4c019a02291a97f0acc5dd5a31bc5
Dr. Andreas Plagens <andreas@plagens.eu>
2021-10-01 22:33:59 +0200

    Initial Commit

---
+++ AnimationExplorer/AnimationExplorerApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationExplorerApp.swift
+//  AnimationExplorer
+//
+//  Created by Dr. Andreas Plagens on 01.10.21.
+//
+
+import SwiftUI
+
+@main
+struct AnimationExplorerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
