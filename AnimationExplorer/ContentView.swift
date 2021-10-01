292ee355076d2db50f763a504ef60acde3ef6be1
Dr. Andreas Plagens <andreas@plagens.eu>
2021-10-02 00:12:16 +0200

    trial

--- AnimationExplorer/ContentView.swift
+++ AnimationExplorer/ContentView.swift
@@ -8,9 +8,35 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var anim = false
+    @State var opac = false
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            
+            Spacer()
+            
+            Text("Hello, world!")
+                .font(.largeTitle)
+                .rotationEffect(Angle.degrees(anim ? 360 : 0))
+                .animation(anim ? Animation.linear(duration: 1).repeatForever(autoreverses: false) : Animation.linear(duration: 0))
+                .opacity(opac ? 0 : 100 )
+                .padding(20)
+            
+            
+            Spacer()
+            
+            Button("Animation") {
+                anim.toggle()
+            }
+            
+            Spacer()
+            
+            Button("Opacity") {
+                opac.toggle()
+            }
+            Spacer()
+        }
     }
 }
 
@@ -19,3 +45,4 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
